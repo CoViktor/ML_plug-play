1e2051d1d2b211ac73b76b0adb23102fc26613b5
Viktor Cosaert <viktor.cosaert@gmail.com>
2024-05-10 14:01:19 +0200

    bivariates script updated

--- EDA/bivariates.py
+++ EDA/bivariates.py
@@ -1,29 +1,8 @@
-import plotly as py
-import plotly.express as px
-
-
-
-
-# get imput on what target variable is
-# -> do bivariates for each var & target
-# plot numericals & categoricals in their own way
-# -> look for the covar prints of each numerical var with target
-
-# ALSO DEPENDS ON TYPE OF TARGET
-
-# Bivariates correlation matrix
-# first OHE the categorical columns
-
-def plot_target_bivariates(df, target, type, columns=None):
-    pass
-
-
 import pandas as pd
+import plotly.figure_factory as ff
 import plotly.express as px
-import seaborn as sns
+from sklearn.preprocessing import OneHotEncoder
 from scipy.stats import pearsonr
-from scipy.stats import chisquare
-import numpy as np
 
 
 def num_y_bivariates(df, y, columns):
@@ -47,6 +26,41 @@ def cat_y_bivariates(df, y, columns):
             else:
                 fig = px.box(df, x=col, y=y, color=y, title=f'{col} vs {y}')
             fig.show()
+
+
+
+def correlation_matrix_heatmap(df, columns):
+    # Determine categorical columns & OHE
+    categorical_columns = df[columns].select_dtypes(include=['object', 'category']).columns
+    encoder = OneHotEncoder(handle_unknown='ignore')
+    encoded_data = encoder.fit_transform(df[categorical_columns]).toarray()
+    feature_names = encoder.get_feature_names_out()
+    df_encoded = df.drop(columns=categorical_columns)
+    df_encoded = pd.concat([df_encoded, pd.DataFrame(encoded_data, columns=feature_names, index=df.index)], axis=1)
+    
+    # Calculate the correlation matrix
+    correlation_matrix = df_encoded.corr()
+    
+    # Plot the heatmap
+    fig = ff.create_annotated_heatmap(
+        z=correlation_matrix.values,
+        x=list(correlation_matrix.columns),
+        y=list(correlation_matrix.index),
+        annotation_text=correlation_matrix.round(2).values,
+        showscale=True,
+        colorscale='Viridis'
+    )
+    fig.show()
+    
+    # Print significant correlations
+    for feature_name in df_encoded.columns:
+        if pd.api.types.is_numeric_dtype(df_encoded[feature_name]):
+            valid_idx = df_encoded[feature_name].notna()
+            for target_column in df_encoded.columns:
+                if target_column != feature_name:
+                    corr, _ = pearsonr(df_encoded.loc[valid_idx, feature_name], df_encoded.loc[valid_idx, target_column])
+                    if abs(corr) >= 0.5:
+                        print(f"Significant correlation between {feature_name} and {target_column}: {round(corr, 2)}")
 
 
 def explore_bivariates(df, target, columns=None):
